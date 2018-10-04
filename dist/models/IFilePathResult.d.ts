44ef21478a2e3817bf3ee21a680f3918bd70ddfc
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-10-04 12:22:10 +0530

    wip

---
+++ dist/models/IFilePathResult.d.ts
@@ -0,0 +1,4 @@
+import IFilePath from "./IFilePath";
+export default interface IFilePathResult {
+    files: IFilePath[];
+}
---
+++ src/models/IFilePathResult.ts
@@ -0,0 +1,7 @@
+import IFilePath from "./IFilePath";
+
+export default interface IFilePathResult {
+
+    files: IFilePath[];
+
+}
