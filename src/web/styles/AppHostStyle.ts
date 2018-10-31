4183ead7fcfe3f2e4a92533473f006777f7d1d31
NS\Hitesh.p <hitesh.neurospeech@gmail.com>
2018-10-31 16:21:06 +0530

    AppHost redesigned

--- src/web/styles/AppHostStyle.ts
+++ src/web/styles/AppHostStyle.ts
@@ -5,7 +5,45 @@ export default class AppHostStyle extends AtomStyle {
 
     public get root(): IStyleDeclaration {
         return {
-            fontFamily: "arial"
+            fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans," +
+            "Ubuntu, Cantarell, 'Helvetica Neue', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+            fontSize: "14px",
+            color: "#3c4144",
+            subclasses: {
+                " .header": {
+                    padding: "10px",
+                    background: "#f5f5f5",
+                    subclasses: {
+                        " input": {
+                            border: "1px solid #bbb",
+                            padding: "5px",
+                            borderRadius: "3px"
+                        },
+                        " button": {
+                            border: "1px solid #13a538",
+                            backgroundColor: "#13a538",
+                            color: "#f2fff5",
+                            padding: "5px",
+                            borderRadius: "3px",
+                            marginTop: "5px"
+                        },
+                        " .topnav-right": {
+                            float: "right",
+                            marginTop: "3px",
+                            subclasses: {
+                                " a": {
+                                    backgroundColor: "#168eea",
+                                    borderRadius: "3px",
+                                    padding: "5px 10px",
+                                    color: "white",
+                                    textDecoration: "none",
+                                    cursor: "pointer"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         };
     }
 
--- src/web/views/AppHost.ts
+++ src/web/views/AppHost.ts
@@ -42,10 +42,13 @@ export default  class AppHost extends AtomGridView {
         
         this.append(e2);
         
-        this.setPrimitiveValue(e2, "row", "0:2" );
+        this.setPrimitiveValue(e2, "row", "0" );
         
 
-        this.setPrimitiveValue(e2, "style", "padding:5px" );
+        this.setPrimitiveValue(e2, "column", "0:2" );
+        
+
+        this.setPrimitiveValue(e2, "class", "header" );
         
         
         const e3 = document.createTextNode("\r\n        ");
@@ -69,111 +72,150 @@ export default  class AppHost extends AtomGridView {
         
         e2.appendChild(e5);
 
-        const e6 = document.createElement("button");
+        const e6 = document.createElement("div");
         
         e2.appendChild(e6);
         
-            this.runAfterInit( () =>
-            this.setLocalValue(e6, "eventClick", () => (this.viewModel).refreshUrl()) );
+        this.setPrimitiveValue(e6, "class", "topnav-right" );
         
-        const e7 = document.createTextNode("Refresh");
+        
+        const e7 = document.createTextNode("\r\n            ");
         
         e6.appendChild(e7);
 
-        const e8 = document.createTextNode("\r\n        ");
+        const e8 = document.createElement("a");
         
-        e2.appendChild(e8);
+        e6.appendChild(e8);
+        
+            this.bind(e8, "href",  [["viewModel","url"]], false , (v1) => (v1) );
 
-        const e9 = document.createElement("a");
+        this.setPrimitiveValue(e8, "target", "_tab" );
         
-        e2.appendChild(e9);
         
-            this.bind(e9, "href",  [["viewModel"],["viewModel","url"]], false , (v1,v2) => (v1).inspect((v2)) );
+        const e9 = document.createTextNode("Open New Tab");
+        
+        e8.appendChild(e9);
 
-        this.setPrimitiveValue(e9, "target", "_tab" );
+        const e10 = document.createTextNode("\r\n            ");
         
-        
-        const e10 = document.createTextNode("Inspect");
-        
-        e9.appendChild(e10);
+        e6.appendChild(e10);
 
-        const e11 = document.createTextNode("\r\n    ");
+        const e11 = document.createElement("a");
         
-        e2.appendChild(e11);
+        e6.appendChild(e11);
+        
+            this.bind(e11, "href",  [["viewModel"],["viewModel","url"]], false , (v1,v2) => (v1).inspect((v2)) );
+        
+        const e12 = document.createTextNode("Inspect");
+        
+        e11.appendChild(e12);
 
-        const e12 = document.createTextNode("\r\n\r\n    ");
+        const e13 = document.createTextNode("\r\n            ");
         
-        this.element.appendChild(e12);
+        e6.appendChild(e13);
 
-        const e13 = document.createElement("div");
+        const e14 = document.createElement("a");
         
-        this.append(e13);
+        e6.appendChild(e14);
         
-        this.setPrimitiveValue(e13, "style", "overflow: auto; width: 100%; height: 100%" );
+            this.runAfterInit( () =>
+            this.setLocalValue(e14, "eventClick", () => (this.viewModel).refreshUrl()) );
         
+        const e15 = document.createTextNode("Refresh");
+        
+        e14.appendChild(e15);
 
-        this.setPrimitiveValue(e13, "row", "1" );
+        const e16 = document.createTextNode("\r\n        ");
         
-        
-        const e14 = document.createTextNode("\r\n        ");
-        
-        e13.appendChild(e14);
-
-            const e15 = new AtomListBox(this.app);
-            
-            
-        const e16 = document.createTextNode("\r\n            ");
-        
-        e15.element.appendChild(e16);
+        e6.appendChild(e16);
 
         const e17 = document.createTextNode("\r\n        ");
         
-        e15.element.appendChild(e17);
+        e2.appendChild(e17);
+
+        const e18 = document.createElement("undefined");
+        
+        e2.appendChild(e18);
+        
+        
+
+        const e19 = document.createTextNode("\r\n    ");
+        
+        e2.appendChild(e19);
+
+        const e20 = document.createTextNode("\r\n\r\n    ");
+        
+        this.element.appendChild(e20);
+
+        const e21 = document.createElement("div");
+        
+        this.append(e21);
+        
+        this.setPrimitiveValue(e21, "style", "overflow: auto; width: 100%; height: 100%" );
+        
+
+        this.setPrimitiveValue(e21, "row", "1" );
+        
+        
+        const e22 = document.createTextNode("\r\n        ");
+        
+        e21.appendChild(e22);
+
+            const e23 = new AtomListBox(this.app);
             
-            e15.bind(e15.element, "items",  [["viewModel","files"]], false , (v1) => (v1) );
-
-        e15.setPrimitiveValue(e15.element, "valuePath", "url" );
-        
-
-            e15.bind(e15.element, "value",  [["viewModel","url"]], true  );
-
-        e15.itemTemplate = AppHost_itemTemplate_1_1Creator(this);
             
-            e13.appendChild(e15.element);
-
-
-        const e18 = document.createTextNode("\r\n    ");
+        const e24 = document.createTextNode("\r\n            ");
         
-        e13.appendChild(e18);
+        e23.element.appendChild(e24);
 
-        const e19 = document.createTextNode("\r\n\r\n    ");
+        const e25 = document.createTextNode("\r\n        ");
         
-        this.element.appendChild(e19);
+        e23.element.appendChild(e25);
+            
+            e23.bind(e23.element, "items",  [["viewModel","files"]], false , (v1) => (v1) );
 
-        const e20 = document.createElement("iframe");
-        
-        this.append(e20);
-        
-        this.setPrimitiveValue(e20, "row", "1" );
+        e23.setPrimitiveValue(e23.element, "valuePath", "url" );
         
 
-        this.setPrimitiveValue(e20, "column", "1" );
+            e23.bind(e23.element, "value",  [["viewModel","url"]], true  );
+
+        e23.itemTemplate = AppHost_itemTemplate_1_7Creator(this);
+            
+            e21.appendChild(e23.element);
+
+
+        const e26 = document.createTextNode("\r\n    ");
+        
+        e21.appendChild(e26);
+
+        const e27 = document.createTextNode("\r\n\r\n    ");
+        
+        this.element.appendChild(e27);
+
+        const e28 = document.createElement("iframe");
+        
+        this.append(e28);
+        
+        this.setPrimitiveValue(e28, "row", "1" );
         
 
-            this.bind(e20, "src",  [["viewModel","url"]], false , (v1) => (v1) );
-
-        this.setPrimitiveValue(e20, "style", "border: none; padding:5px; width:100%; height:100%;" );
-        
+        this.setPrimitiveValue(e28, "column", "1" );
         
 
-        const e21 = document.createTextNode("\r\n\r\n");
+            this.bind(e28, "src",  [["viewModel","url"]], false , (v1) => (v1) );
+
+        this.setPrimitiveValue(e28, "style", "border: none; padding:5px; width:100%; height:100%;" );
         
-        this.element.appendChild(e21);
+        
+
+        const e29 = document.createTextNode("\r\n\r\n");
+        
+        this.element.appendChild(e29);
                 }
             }
 
-            function AppHost_itemTemplate_1_1Creator(__creator){
-                return  class AppHost_itemTemplate_1_1 extends AtomControl {
+            function AppHost_itemTemplate_1_7Creator(__creator){
+                return  class AppHost_itemTemplate_1_7 extends AtomControl {
 
                 
 
@@ -202,6 +244,9 @@ export default  class AppHost extends AtomGridView {
 
             this.runAfterInit( () =>
             this.setLocalValue(e2, "title", ((this.data) ? this.data.dir : undefined)) );
+
+        this.setPrimitiveValue(e2, "style", "font-weight: 500;" );
+        
         
 
         const e3 = document.createTextNode("\r\n                ");
@@ -212,16 +257,19 @@ export default  class AppHost extends AtomGridView {
         
         this.append(e4);
         
-        this.setPrimitiveValue(e4, "style", "font-size: small" );
+        this.setPrimitiveValue(e4, "style", "font-size: small;" );
         
 
             this.runAfterInit( () =>
             this.setLocalValue(e4, "text", ((this.data) ? this.data.dir : undefined)) );
         
-
-        const e5 = document.createTextNode("\r\n            ");
+        const e5 = document.createTextNode("\r\n                ");
         
-        this.element.appendChild(e5);
+        e4.appendChild(e5);
+
+        const e6 = document.createTextNode("\r\n            ");
+        
+        this.element.appendChild(e6);
                 }
             }
 
