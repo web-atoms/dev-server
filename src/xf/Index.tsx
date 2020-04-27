2ed596626546975f96d512d206feb82c411eb580
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-04-27 10:22:01 +0530

    Added search in view list

--- src/xf/Index.tsx
+++ src/xf/Index.tsx
@@ -14,31 +14,42 @@ export default class Index extends AtomXFContentPage {
 
         this.render(<XF.ContentPage
                 title="Search View">
-            <XF.ListView
-                hasUnevenRows={true}
-                rowHeight={70}
-                itemsSource={Bind.oneWay(() => this.viewModel.files)}>
-                <XF.ListView.itemTemplate>
-                    <XF.DataTemplate>
-                        <XF.ViewCell>
-                    <XF.StackLayout padding={10}>
-                        <XF.StackLayout.gestureRecognizers>
-                            <XF.TapGestureRecognizer
-                                command={Bind.event((x) => this.viewModel.openUrl(x.data))}/>
-                        </XF.StackLayout.gestureRecognizers>
-                        <XF.Label
-                            text={Bind.oneWay((x) => x.data.name)}
-                            fontSize={14}
-                            textColor="#2e2e2e"
-                            />
-                        <XF.Label Text={Bind.oneWay((x) => x.data.dir)}
+            <XF.Grid>
+                <XF.Grid.rowDefinitions>
+                    <XF.RowDefinition height="auto"/>
+                    <XF.RowDefinition/>
+                </XF.Grid.rowDefinitions>
+                <XF.SearchBar
+                    text={Bind.twoWays(() => this.viewModel.search)}
+                    />
+                <XF.ListView
+                    { ...  XF.Grid.row(1)}
+                    cachingStrategy="RecycleElement"
+                    hasUnevenRows={true}
+                    rowHeight={70}
+                    itemsSource={Bind.oneWay(() => this.viewModel.files)}>
+                    <XF.ListView.itemTemplate>
+                        <XF.DataTemplate>
+                            <XF.ViewCell>
+                            <XF.StackLayout padding={10}>
+                                <XF.StackLayout.gestureRecognizers>
+                                    <XF.TapGestureRecognizer
+                                        command={Bind.event((x) => this.viewModel.openUrl(x.data))}/>
+                                </XF.StackLayout.gestureRecognizers>
+                            <XF.Label
+                                text={Bind.oneWay((x) => x.data.name)}
                                 fontSize={14}
-                                textColor="#0000EE"/>
-                    </XF.StackLayout>
-                    </XF.ViewCell>
+                                textColor="#2e2e2e"
+                                />
+                            <XF.Label Text={Bind.oneWay((x) => x.data.dir)}
+                                    fontSize={14}
+                                    textColor="#0000EE"/>
+                            </XF.StackLayout>
+                        </XF.ViewCell>
                     </XF.DataTemplate>
                 </XF.ListView.itemTemplate>
             </XF.ListView>
+            </XF.Grid>
         </XF.ContentPage>);
     }
 }
